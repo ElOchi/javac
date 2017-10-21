41f04c9c43b641b2eb145c9bc536767f33cebe94
El Ochi <dantezell78@gmail.com>
2017-10-20 21:37:19 -0400

    second

---
+++ Java Basic/multiple_strings.java
@@ -0,0 +1,18 @@
+import java.io.Console;
+
+public class TreeStory {
+
+  public static void main(String[] args) {
+    Console console = System.console();
+
+    String anyQuestions = console.readLine("Are there any questions?  ");
+    while (anyQuestions.equals("yes")) {
+      String question = console.readLine("What is your question?  ");
+      console.printf("I do not understand:  %s \n", question );
+      anyQuestions = console.readLine("Are there any more questions ");
+    }
+
+    console.printf("Next slide...");
+
+  }
+}
